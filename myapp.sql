8a89264eb7ab3529b5f15bae06606d003ddbb2cf
”名前” <>
2020-05-12 07:33:26 +0000

    sqlの学習3

--- myapp.sql
+++ myapp.sql
@@ -1,14 +1,29 @@
-drop table if exists users;
-create table users (
-    name varchar(20),
-    score float
+drop table if exists posts;
+create table posts (
+    id int unsigned primary key auto_increment,
+    title varchar(255),
+    body text
+);
+drop table if exists comments;
+create table comments (
+    id int unsigned primary key auto_increment,
+    post_id int not null,
+    body text
 );
 
-alter table users add index index_score (score);
-show index from users;
+insert into posts (title, body) values ('title1', 'body1');
+insert into posts (title, body) values ('title2', 'body2');
+insert into posts (title, body) values ('title3', 'body3');
 
--- explain select * from users where score > 5.0;
--- explain select * from users where name > "taguchi";
+insert into comments (post_id, body) values (1, 'first comments for post1');
+insert into comments (post_id, body) values (1, 'second comments for post1');
+insert into comments (post_id, body) values (3, 'first comments for post3');
+insert into comments (post_id, body) values (4, 'first comments for post4');
 
-alter table users drop index index_score;
-show index from users;
+select * from posts;
+select * from comments;
+
+-- select * from posts  join comments on posts.id = comments.post_id;
+-- select * from posts  join comments on posts.id = comments.post_id;
+select posts.id, title, posts.body, comments.body from posts  join comments on posts.id = comments.post_id;
+
